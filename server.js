335ecc11730b97a0d0cdc17335c4ad4da3fa9e46
Jess Daniel <jessdaniel474@gmail.com>
2019-12-04 13:12:26 -0500

    adding custom middleware

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-const express = require('express'); // importing a CommonJS module
+/**
+ * @prettier
+ */
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
+
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
+// Middlewares
+server.use(helmet());
 server.use(express.json());
 
-server.use('/api/hubs', hubsRouter);
+// custom middleware
+function validatePassword(req, res, next) {
+  const password = req.headers.password;
+  if (password === "melon") {
+    next();
+  } else {
+    res.status(401).json({ error: "invalid password" });
+  }
+}
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.use("/api/hubs", hubsRouter);
+
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
+});
+
+server.get("/area51", validatePassword(), (req, res) => {
+  res.send(req.headers);
 });
 
 module.exports = server;
